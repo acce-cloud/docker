eca4927bf455fdd5ff8fc10335df9577922c40df
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-09-26 17:33:21 -0600

    Writing a more interesting filename.

--- examples/example-swarm/pges/writeBin.py
+++ examples/example-swarm/pges/writeBin.py
@@ -1,17 +1,27 @@
 # Python program that writes a binary file of N number of MB
 # The output file is written to the directory $OUTPUT_DIR if defined,
 # otherwise in the current directory where the program is executed.
+#
+# Usage:
+# python writeBin.py day# orbit# scene#
 
-from array import array
+import sys
 import os
+from array import array
 
 size_in_mb = 10
+
+# parse command line arguments
+day   = sys.argv[1]
+orbit = sys.argv[2]
+scene = sys.argv[3]
 
 # output directory, defaults to current directory
 output_dir = os.environ.get("OUTPUT_DIR", os.environ["PWD"])
 
 # output file
-output_file = open(os.path.join(output_dir,'file.out'), 'wb')
+file_name = "output_d%s_o%s_s%s.out" % (day, orbit, scene)
+output_file = open(os.path.join(output_dir, file_name), 'wb')
 
 # each loop iteration will write out 1 KB of data
 for i in range(size_in_mb*1024): # loop over KB
