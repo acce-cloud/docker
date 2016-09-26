8c3e3e36d32620c5e270c84de3cfbb12d1ba4ded
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-09-26 07:58:05 -0600

    Updating the build all script.

--- docker_build_all.sh
+++ docker_build_all.sh
@@ -3,13 +3,30 @@
 # Usage:
 # ./docker_build_all.sh [--push] 
 
+function build_and_push() {
+
+  # function parameters
+  img=$1
+  pushit=$2
+  echo "\nBUILDING MODULE=$img PUSH=$pushit\n"
+
+  # build the module
+  cd $img
+  docker build -t oodthub/$img .
+
+  # optionally push the module to Docker Hub
+  if [ $pushit == '--push' ]; then
+       docker push oodthub/$img
+  fi
+
+}
+
 # optional 'push' argument
 pushit=${1:-false}
 
-echo "$pushit"
+# loop over ordered list of OODT images
+images=('oodt-node' 'oodt-filemgr' 'oodt-wmgr' 'oodt-resmgr' 'oodt-fmprod' 'oodt-crawler')
 
-cd oodt-node
-docker build -t oodthub/oodt-node .
-if [ $pushit == '--push' ]; then
-	docker push oodthub/oodt-node
-fi
+for img in ${images[*]}; do
+   build_and_push $img $pushit
+done
