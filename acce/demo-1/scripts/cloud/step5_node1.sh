d60019ede0b3a82350ebb4aba2544933ab274edb
ACCE Deploy <adeploy@acce-build1.dyndns.org>
2016-11-02 10:40:40 -0700

    Sleep some more before querying solr.

--- acce/demo-1/scripts/cloud/step5_node1.sh
+++ acce/demo-1/scripts/cloud/step5_node1.sh
@@ -10,4 +10,5 @@ sleep 5
 docker exec -it ${wmgr_client_id} sh -c "cd /usr/local/oodt/cas-workflow/bin; ./wmgr-client --url http://wmgr:9001 --operation --sendEvent --eventName test-workflow --metaData --key Dataset abc --key Project 123 --key Run 2"
 sleep 5
 
+sleep 5
 curl "http://${MANAGER_IP}:8983/solr/oodt-fm/select?q=*%3A*&wt=json&indent=true&rows=0"
