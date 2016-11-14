8341189543bee659f079e584ca5ff354527324fd
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-11-14 09:28:00 -0700

    Labcas swarm demo.

--- labcas/swarm-demo/step4_node1.sh
+++ labcas/swarm-demo/step4_node1.sh
@@ -8,14 +8,38 @@
 mkdir -p /usr/local/adeploy/pges
 mkdir -p /usr/local/adeploy/archive
 
-docker service create --replicas 1 --name labcas-wmgr \
-                      -p 9000:9000 -p 8983:8983 --network swarm-network \
-                      --mount type=bind,src=/usr/local/adeploy/pges,dst=/usr/local/oodt/pges\
-                      --mount type=bind,src=/usr/local/adeploy/archive,dst=/usr/local/oodt/archive\
-                      oodthub/labcas-biomarker-discovery\
-                      /usr/bin/supervisord -c /etc/supervisor/supervisord-wmgr.conf
+docker node update --label-add oodt_type=filemgr acce-build1.dyndns.org
+docker node update --label-add oodt_type=wmgr acce-build2.dyndns.org
+docker node update --label-add oodt_type=wmgr acce-build3.dyndns.org
 
-docker service scale labcas-wmgr=3
+# start services: same images, different servers
+docker service create --replicas 1 --name labcas-wmgr \
+                      -p 9001:9001 --network swarm-network \
+                      --mount type=bind,src=/usr/local/adeploy/pges,dst=/usr/local/oodt/pges \
+                      --mount type=bind,src=/usr/local/adeploy/archive,dst=/usr/local/oodt/archive \
+                      --env FILEMGR_URL=http://labcas-filemgr:9000 \
+                      --constraint 'node.labels.oodt_type==wmgr' \
+                      oodthub/labcas-biomarker-discovery \
+                      /usr/bin/supervisord -c /etc/supervisor/supervisord-workflow.conf
+
+docker service create --replicas 1 --name labcas-filemgr \
+                      -p 9000:9000 -p 8983:8983 --network swarm-network \
+                      --mount type=bind,src=/usr/local/adeploy/archive,dst=/usr/local/oodt/archive \
+                      --constraint 'node.labels.oodt_type==filemgr' \
+                      oodthub/labcas-biomarker-discovery \
+                      /usr/bin/supervisord -c /etc/supervisor/supervisord-filemgr.conf
+
+docker service create --replicas 1 --name labcas-wmgr-client \
+                      --network swarm-network \
+                      --env WORKFLOW_URL=http://labcas-wmgr:9000 \
+                      --constraint 'node.labels.oodt_type==filemgr' \
+                      oodthub/labcas-biomarker-discovery \
+                      tail -f /dev/null
+
+
+docker service scale wmgr=3
 
 docker service ls
 docker service ps labcas-wmgr
+docker service ps labcas-filemgr
+docker service ps labcas-wmgr-client
