51561f1ccf842cad6eff70f6548b102ea39bbd28
ACCE Deploy <adeploy@acce-build1.dyndns.org>
2016-11-16 01:46:01 -0800

    Moving scripts around.

--- labcas/labcas-biomarker-discovery/scripts/cloud/step5_node1.sh
+++ labcas/labcas-biomarker-discovery/scripts/cloud/step5_node1.sh
@@ -6,7 +6,7 @@
 wmgr_client_id=`docker ps | grep labcas-wmgr-client | awk '{print $1}'`
 
 # submit NCV workflows
-NCV=2
+NCV=10
 for i in `seq 1 $NCV`
 do
   docker exec -it ${wmgr_client_id} sh -c "cd /usr/local/oodt/cas-workflow/bin; ./wmgr-client --url http://labcas-wmgr:9001 --operation --sendEvent --eventName biomarker-discovery --metaData --key CrossValidationIterationNumber $i --key TrainingSet GSE4115_10female_10male.rds"
