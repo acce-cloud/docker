aeda612b2bc92b0805ad5439f5277db504d55133
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2017-01-04 14:30:50 -0700

    iAdding python producer and consumer.

---
+++ oodt-rabbitmq/clients/workflow_consumer.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# Python workflow consumer client:
+# receives messages from the RabbitMQ server to start OODT workflows
+# Usage: # Usage: python workflow_producer.py <workflow_event>
+
+import pika
+import sys
+import os
+
+def callback(ch, method, properties, body):
+    print("Submitting workflow %r: %r" % (method.routing_key, body))
+
+# connect to RabbitMQ server: use RABBITMQ_URL or default to guest/guest @ localhost
+url = os.environ.get('RABBITMQ_URL', 'amqp://guest:guest@localhost/%2f')
+params = pika.URLParameters(url)
+params.socket_timeout = 5
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+
+# use common OODT workflows Exchange
+channel.exchange_declare(exchange='oodt_workflows', type='direct')
+
+# declare one temporary queue per consumer
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
+
+# parse command line argument
+if len(sys.argv) < 2:
+  raise Exception("Usage: python workflow_consumer.py <workflow_event>")
+else:
+  workflow_event = sys.argv[1]
+
+# bind queue to exchange with appropriate binding key
+channel.queue_bind(exchange='oodt_workflows',
+                   queue=queue_name,
+                   routing_key=workflow_event)
+
+# start consuming
+print('Waiting for workflow events. To exit press CTRL+C')
+channel.basic_consume(callback, queue=queue_name, no_ack=True)
+channel.start_consuming()
---
+++ oodt-rabbitmq/clients/workflow_producer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# Python workflow producer client:
+# sends a message to the RabbitMQ server to start a workflow
+# Usage: python workflow_producer.py <workflow_event> [<metadata_key=metadata_value> <metadata_key=metadata_value> ...]
+
+import sys
+import os
+import pika
+
+# connect to RabbitMQ server: use RABBITMQ_URL or default to guest/guest @ localhost
+url = os.environ.get('RABBITMQ_URL', 'amqp://guest:guest@localhost/%2f')
+params = pika.URLParameters(url)
+params.socket_timeout = 5
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+
+# use common OODT workflows Exchange
+channel.exchange_declare(exchange='oodt_workflows', type='direct')
+
+# parse command line arguments
+if len(sys.argv) < 2:
+  raise Exception("Usage: python workflow_producer.py <workflow_event> [<metadata_key=metadata_value> <metadata_key=metadata_value> ...]")
+else:
+  workflow_event = sys.argv[1]
+  message = ' '.join(sys.argv[2:]) or ''
+
+# produce message
+channel.basic_publish(exchange='oodt_workflows',
+                      routing_key=workflow_event,
+                      body=message)
+
+print(" [x] Sent workflow message %r: %r" % (workflow_event, message))
+connection.close()
