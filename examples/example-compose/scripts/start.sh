3f704d7658cb5d97ec7ba61dfb05cb856f337bb7
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2017-01-17 16:47:27 -0700

    Updating the example compose.

--- examples/example-compose/scripts/start.sh
+++ examples/example-compose/scripts/start.sh
@@ -9,11 +9,12 @@ num_workflow_clients=${args[1]}
 cd $OODT_HOME/cas-workflow/bin
 ./wmgr start
 
+# wait for rabbitmq server to finish initialization of OODT user accounts
+sleep 5
 # start the rabbitmq consumers
 # will listen to standard output and keep the container running
-# wait for rabbitmq server to finish iinitialization of OODT user accounts
-sleep 5
-python /usr/local/oodt/rabbitmq/workflow_consumer.py $workflow_event $num_workflow_clients
+#python /usr/local/oodt/rabbitmq/workflow_consumer.py $workflow_event $num_workflow_clients
+python /usr/local/oodt/rabbitmq/rabbitmq_consumer.py $workflow_event $num_workflow_clients
 
 # keep container running
 #tail -f $OODT_HOME/cas-workflow/logs/cas_workflow.log
