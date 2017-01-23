59bcf51fce3beef61bf85764c3595e8c42298f47
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2017-01-23 12:36:08 -0700

    Updating workflow to use message_counter.

--- examples/example-swarm-mode/scripts/cloud/step5_node1.sh
+++ examples/example-swarm-mode/scripts/cloud/step5_node1.sh
@@ -4,10 +4,7 @@
 
 rabbitmq_id=`docker ps | grep rabbitmq | awk '{print $1}'`
 
-docker exec -it ${rabbitmq_id} sh -c "cd /usr/local/oodt/rabbitmq; python rabbitmq_producer.py test-workflow 10 Dataset=abc Project=123 Run=1"
-
-sleep 3
-docker exec -it ${rabbitmq_id} sh -c "cd /usr/local/oodt/rabbitmq; python rabbitmq_producer.py test-workflow 10 Dataset=abc Project=123 Run=2"
+docker exec -it ${rabbitmq_id} sh -c "cd /usr/local/oodt/rabbitmq; python rabbitmq_producer.py test-workflow 10 Dataset=abc Project=123"
 
 sleep 10
 curl "http://${MANAGER_IP}:8983/solr/oodt-fm/select?q=*%3A*&wt=json&indent=true&rows=0"
