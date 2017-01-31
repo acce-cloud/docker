66b4bb51ab340d6afc7ebed882eb5aceb22c2481
ACCE Deploy <adeploy@acce-build1.dyndns.org>
2017-01-31 11:25:46 -0800

    Working on nohup script.

---
+++ examples/example-swarm-mode/scripts/cloud/step5b_node1.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# node: acce-build1.dyndns.org
+# Submits the OODT "test-workflow" (twice), then queries the Solr catalog
+
+# define URLs to access the RabbitMQ server
+export RABBITMQ_HOST_IP=172.31.4.166
+export RABBITMQ_USER_URL=amqp://oodt-user:changeit@${RABBITMQ_HOST_IP}/%2f
+export RABBITMQ_ADMIN_URL=http://oodt-admin:changeit@${RABBITMQ_HOST_IP}:15672
+
+# execute local script to send messages to RabbitMQ server
+python ../../wmgr_config/rabbitmq_clients/rabbitmq_producer.py test-workflow 1 Dataset=abc Project=123 heap=1 size=10 time=10
+
+#sleep 10
+#curl "http://${RABBITMQ_HOST_IP}:8983/solr/oodt-fm/select?q=*%3A*&wt=json&indent=true&rows=0"
+# to delete all records:
+# curl "http://${RABBITMQ_HOST_IP}:8983/solr/oodt-fm/update?stream.body=<delete><query>*:*</query></delete>&commit=true"
