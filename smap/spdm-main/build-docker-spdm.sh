2326fe5a092c4035b36b7079f526c5ef61ec2d54
Cynthia L Wong <clwong@eco-scf.jpl.nasa.gov>
2017-02-09 19:38:14 +0000

    Fixed docker commands with "sudo docker"

--- smap/spdm-main/build-docker-spdm.sh
+++ smap/spdm-main/build-docker-spdm.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 if [ -r ./credentials.sh ] ; then 
    . ./credentials.sh
+else
+   echo "Please copy build/credentials.sh template to this directory & add your credentials"
 fi
 sudo docker build -t oodthub/spdm-services:0.3 \
               --build-arg jpl_git_user=${JPL_GIT_USER} \
