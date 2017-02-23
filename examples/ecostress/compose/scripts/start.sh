9b66f7b67b98991c68fb3bc27c1bf80a7bcf646c
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2017-02-23 13:07:09 -0700

    Changing start.sh script.

--- examples/ecostress/compose/scripts/start.sh
+++ examples/ecostress/compose/scripts/start.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-# parse command line arguments
-args=("$@")
-workflow_event=${args[0]}
-num_workflow_clients=${args[1]}
-
 # OODT Workflow Manager
 cd $OODT_HOME/cas-workflow/bin
 ./wmgr start
 
 # wait for rabbitmq server to finish initialization of OODT user accounts
 sleep 5
+
 # start the rabbitmq consumers
-# will listen to standard output and keep the container running
-#python /usr/local/oodt/rabbitmq/workflow_consumer.py $workflow_event $num_workflow_clients
-python /usr/local/oodt/rabbitmq/rabbitmq_consumer.py $workflow_event $num_workflow_clients
+# one consumer per workflow specified as script argument
+for arg in "$@"; do
+  echo "RabbitMQ consumer listening to queue ${arg}..."
+  python /usr/local/oodt/rabbitmq/rabbitmq_consumer.py $arg 1 &
+done
 
 # keep container running
-#tail -f $OODT_HOME/cas-workflow/logs/cas_workflow.log
+tail -f $OODT_HOME/cas-workflow/logs/cas_workflow.log
