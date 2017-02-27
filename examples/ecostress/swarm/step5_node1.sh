45ce0e48bb2a9fc442c378ceb0393d8e3f17206e
ACCE Deploy <adeploy@acce-build1.dyndns.org>
2017-02-27 08:30:29 -0800

    Fixing how to identifier wmgr container.

--- examples/ecostress/swarm/step5_node1.sh
+++ examples/ecostress/swarm/step5_node1.sh
@@ -2,7 +2,7 @@
 # node: acce-build1.dyndns.org
 # Submits the ECOSTRESS 'ecostress-L3a-workflow' workflow
 
-wmgr_id=`docker ps | grep wmgr_head_node | awk '{print $1}'`
+wmgr_id=`docker ps | grep wmgr_L3a.1 | awk '{print $1}'`
 
 # execute rabbitmq producer inside workflow manager container
 docker exec -it ${wmgr_id} sh -c "cd /usr/local/oodt/rabbitmq; python ecostress_driver.py 1"
