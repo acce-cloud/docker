bd139171e20ce9cc52b2acc3689a0d0d85dd2f91
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2017-03-20 14:44:44 -0600

    Submitting 15 orbits.

--- examples/ecostress/swarm-acce-cluster/step5_node1.sh
+++ examples/ecostress/swarm-acce-cluster/step5_node1.sh
@@ -5,4 +5,4 @@
 wmgr_id=`docker ps | grep wmgr_L3a.1 | awk '{print $1}'`
 
 # execute rabbitmq producer inside workflow manager container
-docker exec -it ${wmgr_id} sh -c "cd /usr/local/oodt/rabbitmq; python ecostress_driver.py 1"
+docker exec -it ${wmgr_id} sh -c "cd /usr/local/oodt/rabbitmq; python ecostress_driver.py 15"
--- examples/ecostress/swarm-ecostress-cluster/step4_node1.sh
+++ examples/ecostress/swarm-ecostress-cluster/step4_node1.sh
@@ -7,4 +7,4 @@ alias docker='sudo docker'
 wmgr_id=`docker ps | grep wmgr_L3a.1 | awk '{print $1}'`
 
 # execute rabbitmq producer inside workflow manager container
-docker exec -it ${wmgr_id} sh -c "cd /usr/local/oodt/rabbitmq; python ecostress_driver.py 1"
+docker exec -it ${wmgr_id} sh -c "cd /usr/local/oodt/rabbitmq; python ecostress_driver.py 15"
