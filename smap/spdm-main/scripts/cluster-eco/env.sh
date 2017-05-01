4266393cd06f7007eb32eea493c0aba858d65eb9
Cynthia L Wong <clwong@eco-p31.tir>
2017-05-01 21:47:17 +0000

    Fixed setup for running on eco-p31 & eco-p32

--- smap/spdm-main/scripts/cluster-eco/env.sh
+++ smap/spdm-main/scripts/cluster-eco/env.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-MANAGER_IP=172.31.4.166
-MANAGER=acce-build1.dyndns.org
-NODE1=acce-build2.dyndns.org
-NODE2=acce-build3.dyndns.org
-NODE_LIST="${NODE1} ${NODE2}"
-SHARED_DIR=/usr/local/adeploy/smap/spdmops
+MANAGER_IP=172.30.4.62
+MANAGER=eco-p31.tir
+NODE1=eco-p32.tir
+NODE_LIST="${NODE1}"
+SHARED_DIR=/project/dev/clwong/smap/docker-smap
 SWARM_NETWORK=spdm-swarm
+alias docker="sudo docker"
