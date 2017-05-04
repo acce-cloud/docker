041d3e06b1aaed8d82da0a5e963253807a6115c3
Cynthia L Wong <clwong@eco-p31.tir>
2017-05-04 23:02:25 +0000

    Fixed updates of setenv.sh

--- smap/spdm-main/build/upd_setenv.sh
+++ smap/spdm-main/build/upd_setenv.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 if [ -r credentials.sh ] ; then
-   . credentials.sh
+   . ./credentials.sh
 fi
 
 [ ! -z $1 ] && SPDM_DB_USER=$1
@@ -20,16 +20,16 @@ if [ -z ${SPDM_DB_USER} ] || [ -z {SPDM_DB_PASS} ] ; then
    exit
 fi
 echo "Updating ${ORIGFILE} with database credentials"
-echo ${SPDM_DB_USER}:${SPDM_DB_PASS}@${DB_HOST}/${DB_INSTANT}
+echo ${SPDM_DB_USER}:${SPDM_DB_PASS}@${SPDM_DB_HOST}:${SPDM_DB_PORT}/${DB_INSTANT}
 NEWFILE=./setenv.sh.new
 # refresh back to original file
 [ -e ${ORIGFILE}.orig ] && mv ${ORIGFILE}.orig ${ORIGFILE}
 # update with your credentials
 sed 's/FILEMGR_DB_USER=/FILEMGR_DB_USER='${SPDM_DB_USER}'/' ${ORIGFILE} |  \
 sed 's/FILEMGR_DB_PASS=/FILEMGR_DB_PASS='${SPDM_DB_PASS}'/' | \
-sed 's/DB_HOST=/DB_HOST='${DB_HOST}'/' | \
-sed 's/DB_PORT=/DB_PORT='${DB_PORT}'/' | \
-sed 's/DB_INSTANT=/DB_INSTANT='${DB_INSTANT}'/' > ${NEWFILE}
+sed 's/DB_HOST=/DB_HOST='${SPDM_DB_HOST}'/' | \
+sed 's/DB_PORT=/DB_PORT='${SPDM_DB_PORT}'/' | \
+sed 's/DB_INSTANT=/DB_INSTANT='${SPDM_DB_INSTANT}'/' > ${NEWFILE}
 mv ${ORIGFILE} ${ORIGFILE}.orig
 mv ${NEWFILE} ${ORIGFILE}
-grep FILEMGR_DB ${ORIGFILE}
+grep DB_ ${ORIGFILE}
