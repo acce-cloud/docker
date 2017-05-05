06d081659ef62c2e7cf01c7e0704dacf556e3425
Cynthia L Wong <clwong@eco-p31.tir>
2017-05-05 04:50:01 +0000

    Modified image build script and node host name

--- smap/spdm-main/build-docker-spdm.sh
+++ smap/spdm-main/build-docker-spdm.sh
@@ -4,8 +4,12 @@ if [ -r ./credentials.sh ] ; then
 else
    echo "Please copy build/credentials.sh template to this directory & add your credentials"
 fi
+echo "${JPL_GIT_USER}:${JPL_GIT_TOKEN}:${SPDM_DB_HOST}:${SPDM_DB_PORT}:${SPDM_DB_INSTANT}:${SPDM_DB_USER}:${SPDM_DB_PASS}"
 sudo docker build -t oodthub/spdm-services:0.3 \
               --build-arg jpl_git_user=${JPL_GIT_USER} \
               --build-arg jpl_git_token=${JPL_GIT_TOKEN} \
+              --build-arg spdm_db_host=${SPDM_DB_HOST} \
+              --build-arg spdm_db_port=${SPDM_DB_PORT} \
+              --build-arg spdm_db_instant=${SPDM_DB_INSTANT} \
               --build-arg spdm_db_user=${SPDM_DB_USER} \
               --build-arg spdm_db_pass=${SPDM_DB_PASS} .
--- smap/spdm-main/build/upd_setenv.sh
+++ smap/spdm-main/build/upd_setenv.sh
@@ -4,8 +4,11 @@ if [ -r credentials.sh ] ; then
    . ./credentials.sh
 fi
 
-[ ! -z $1 ] && SPDM_DB_USER=$1
-[ ! -z $2 ] && SPDM_DB_PASS=$2
+[ ! -z $1 ] && SPDM_DB_HOST=$1
+[ ! -z $2 ] && SPDM_DB_PORT=$2
+[ ! -z $3 ] && SPDM_DB_INSTANT=$3
+[ ! -z $4 ] && SPDM_DB_USER=$4
+[ ! -z $5 ] && SPDM_DB_PASS=$5
 
 echo ${PWD}
 ls -l ${PWD}
@@ -16,11 +19,11 @@ if [ -r ${ORIGFILE} ] ; then
    . ${ORIGFILE}
 fi
 if [ -z ${SPDM_DB_USER} ] || [ -z {SPDM_DB_PASS} ] ; then
-   echo "Env SPDM_DB_USER or SPDM_DB_PASS undefined for ${DB_HOST}/${DB_INSTANT}!"
+   echo "Env SPDM_DB_USER or SPDM_DB_PASS undefined for ${SPDM_DB_HOST}:${SPDM_DB_PORT}/${SPDM_DB_INSTANT}!"
    exit
 fi
 echo "Updating ${ORIGFILE} with database credentials"
-echo ${SPDM_DB_USER}:${SPDM_DB_PASS}@${SPDM_DB_HOST}:${SPDM_DB_PORT}/${DB_INSTANT}
+echo ${SPDM_DB_USER}:${SPDM_DB_PASS}@${SPDM_DB_HOST}:${SPDM_DB_PORT}/${SPDM_DB_INSTANT}
 NEWFILE=./setenv.sh.new
 # refresh back to original file
 [ -e ${ORIGFILE}.orig ] && mv ${ORIGFILE}.orig ${ORIGFILE}
