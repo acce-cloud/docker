848c8d62f443f14c73d27178fd718bcb9f249702
ACCE Deploy <adeploy@acce-build1.dyndns.org>
2017-05-12 08:00:42 -0700

    Adding example for one task workflow

---
+++ examples/one-task-test-workflow/swarm/scripts/cloud/step5_node1.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# node: acce-build1.dyndns.org
+# Submits the OODT "test-workflow" (twice), then queries the Solr catalog
+
+rabbitmq_id=`docker ps | grep rabbitmq | awk '{print $1}'`
+
+# execute python client inside RabbitMQ container
+docker exec -it ${rabbitmq_id} sh -c "cd /usr/local/oodt/rabbitmq; python rabbitmq_producer.py test-workflow 100 Dataset=abc Project=123 heap=1 size=10 time=10"
+
+sleep 10
+curl "http://${MANAGER_IP}:8983/solr/oodt-fm/select?q=*%3A*&wt=json&indent=true&rows=0"
+# to delete all records:
+# curl "http://${MANAGER_IP}:8983/solr/oodt-fm/update?stream.body=<delete><query>*:*</query></delete>&commit=true"
