84ea8195b06ff56a4c26786f377dd53b4b2ecd5c
Cynthia L Wong <clwong@eco-p31.tir>
2017-06-15 17:23:55 +0000

    Roll back to use hostname

--- smap/spdm-stub/scripts/docker-entrypoint.sh
+++ smap/spdm-stub/scripts/docker-entrypoint.sh
@@ -17,10 +17,9 @@ cp -p ${SPDM_BUILD}/setenv.sh ${SPDM_HOME}/spdm-filemgr/bin/.
 #   sh spdm_startup.sh ${service}
 #done
 echo "Execute ${SPDM_HOME}/spdm-resource/bin/stub_startup.sh"
-#sh ${SPDM_HOME}/spdm-resource/bin/stub_startup.sh ${SPDM_HOME} `hostname`
-
-HOST=`hostname`
-IP=`nslookup ${HOST} | awk '/^Address: / { print $2 }'`
-
-sh ${SPDM_HOME}/spdm-resource/bin/stub_startup.sh ${SPDM_HOME} ${IP}
+sh ${SPDM_HOME}/spdm-resource/bin/stub_startup.sh ${SPDM_HOME} `hostname`
+# not working right!
+#HOST=`hostname`
+#IP=`nslookup ${HOST} | awk '/^Address: / { print $2 }'`
+#sh ${SPDM_HOME}/spdm-resource/bin/stub_startup.sh ${SPDM_HOME} ${IP}
 tail -f /dev/null
