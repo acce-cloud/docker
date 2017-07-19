4ca7bad921e232876bd38e981c3ef48a2ecb423d
ACCE Deploy <adeploy@acce-build1.dyndns.org>
2017-07-19 08:34:30 -0700

    Changing architecture for the OODT 0.3 test workflow.

--- acce/demo-0.3/swarm-acce-cluster/step4b_node1.sh
+++ acce/demo-0.3/swarm-acce-cluster/step4b_node1.sh
@@ -19,19 +19,34 @@ docker service create --replicas 1 --name oodt-filemgr -p 9000:9000 --network sw
                       --mount type=bind,src=/usr/local/adeploy/pges/test-workflow/jobs,dst=/usr/local/oodt/pges/test-workflow/jobs \
                       oodthub/oodt-filemgr:0.3
 
-# start workflow + resource managers on worker nodes
-docker service create --replicas 2 --name oodt-worker --network swarm-network \
-                      --constraint 'node.labels.oodt_type==worker_node' \
+# start workflow manager on manager node
+docker service create --replicas 2 --name oodt-wmgr -p 9001:9001 --network swarm-network \
+                      --constraint 'node.labels.oodt_type==manager_node' \
                       --mount type=bind,src=/usr/local/adeploy/archive,dst=/usr/local/oodt/archive \
                       --mount type=bind,src=/usr/local/adeploy/src/docker/acce/demo-0.3/workflows/test-workflow/policy,dst=/usr/local/oodt/workflows/policy \
                       --mount type=bind,src=/usr/local/adeploy/src/docker/acce/demo-0.3/workflows/test-workflow/pge-configs,dst=/usr/local/oodt/workflows/test-workflow/pge-configs \
                       --mount type=bind,src=/usr/local/adeploy/src/docker/acce/demo-0.3/pges/test-workflow,dst=/usr/local/oodt/pges/test-workflow \
+                      --mount type=bind,src=/usr/local/adeploy/src/docker/acce/demo-0.3/conf/workflow.properties,dst=/usr/local/oodt/cas-workflow/etc/workflow.properties \
                       --mount type=bind,src=/usr/local/adeploy/pges/test-workflow/jobs,dst=/usr/local/oodt/pges/test-workflow/jobs \
                       --env 'FILEMGR_URL=http://oodt-filemgr:9000/' \
-                      --env 'WORKFLOW_URL=http://localhost:9001' \
-                      --env 'RESMGR_URL=http://localhost:9002' \
+                      --env 'RESMGR_URL=http://oodt-resmgr:9002' \
+                      oodthub/oodt-wmgr:0.3
+#docker service scale oodt-wmgr=2
+
+# start resource manager on worker nodes
+docker service create --replicas 4 --name oodt-resmgr --network swarm-network \
+                      --constraint 'node.labels.oodt_type==worker_node' \
+                      --mount type=bind,src=/usr/local/adeploy/src/docker/acce/demo-0.3/workflows/test-workflow/policy,dst=/usr/local/oodt/workflows/policy \
+                      --mount type=bind,src=/usr/local/adeploy/src/docker/acce/demo-0.3/workflows/test-workflow/pge-configs,dst=/usr/local/oodt/workflows/test-workflow/pge-configs \
+                      --mount type=bind,src=/usr/local/adeploy/src/docker/acce/demo-0.3/pges/test-workflow,dst=/usr/local/oodt/pges/test-workflow \
+                      --mount type=bind,src=/usr/local/adeploy/pges/test-workflow/jobs,dst=/usr/local/oodt/pges/test-workflow/jobs \
+                      --env 'WORKFLOW_URL=http://oodt-wmgr:9001' \
                       --env 'RESMGR_HOME=/usr/local/oodt/cas-resource' \
-                      oodthub/oodt-wmgr-resmgr:0.3
-#docker service scale oodt-worker=4
+                      oodthub/oodt-resmgr:0.3
+#docker service scale oodt-resmgr=2
+
 
 docker service ls
+docker service ps oodt-filemgr
+docker service ps oodt-wmgr
+docker service ps oodt-resmgr
--- acce/demo-0.3/swarm-acce-cluster/step7b_node1.sh
+++ acce/demo-0.3/swarm-acce-cluster/step7b_node1.sh
@@ -2,7 +2,7 @@
 # node: acce-build1.dyndns.org
 # Removes all services from the swarm
 
-docker service rm oodt-filemgr oodt-worker
+docker service rm oodt-filemgr oodt-wmgr oodt-resmgr
 docker service ls
 #docker stop visualizer
 #docker rm visualizer
