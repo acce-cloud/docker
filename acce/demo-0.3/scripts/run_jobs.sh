bef43ab47bf6b017417d142eebc3b4b3d49b4633
EC2 Default User <ec2-user@ip-172-20-12-201.us-west-2.compute.internal>
2017-07-31 19:20:00 +0000

    Inserting latest version of scripts

--- acce/demo-0.3/scripts/run_jobs.sh
+++ acce/demo-0.3/scripts/run_jobs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-NJOBS=1
-SLEEP_SECS=10000
+NJOBS=2000
+NHOSTS=20
+SLEEP_SECS=1
 WORKFLOW_URL=http://internal-EcostressLoadBalancer-1621470833.us-west-2.elb.amazonaws.com:9001
 echo "Running $NJOBS jobs"
 
@@ -15,8 +16,8 @@ for ((i=1;i<=NJOBS;i++)); do
    # submit workflow $i
    echo "Submitting workflow # $i"
    docker exec -it $wrkr_id sh -c "cd /usr/local/oodt/cas-workflow/bin; ./wmgr-client --url $WORKFLOW_URL --operation --sendEvent --eventName test-workflow --metaData --key Dataset abc --key Project 123 --key Run $i"
-   x=$(($i % 100))
-   # wait 60 seconds before submitting the next 100 workflows
+   x=$(($i % $NHOSTS))
+   # wait $SLEEP_SECS seconds before submitting the next 100 workflows
    if [ $x == 0 ]; then
       echo "Sleeping..."
       sleep $SLEEP_SECS
