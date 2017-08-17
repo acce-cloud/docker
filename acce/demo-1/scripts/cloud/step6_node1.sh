878133fcca5a3fdffd267f15d0675782e76b9f39
EC2 Default User <ec2-user@ip-172-20-7-161.us-west-2.compute.internal>
2017-08-17 14:55:17 +0000

    Removing obsolete ACCE demos.

--- acce/demo-1/scripts/cloud/step6_node1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# node: acce-build1.dyndns.org
-# Removes all services from the swarm
-
-docker service rm wmgr-client wmgr filemgr
-docker service ls
-docker stop visualizer
-docker rm visualizer
